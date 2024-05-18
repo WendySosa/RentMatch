1beab4fd6f0632d2b8578be6eac4a80348a44f40
guadalupe sirio <guadalupe.sirio@gmail.com>
2024-05-18 19:47:11 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rent-app',
+  webDir: 'www'
+};
+
+export default config;
